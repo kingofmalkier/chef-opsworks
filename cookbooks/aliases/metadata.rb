20377b71717884b53f1b9c3968ade1e8e05fcec1
Josh Gagnon <jgagnon@rsa.com>
2013-08-12 14:37:06 -0400

    Adding a new recipe we're making ourselves

---
+++ cookbooks/aliases/metadata.rb
@@ -0,0 +1,8 @@
+name             'aliases'
+maintainer       'OneFootSoftware'
+maintainer_email 'josh.gagnon@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures aliases'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends 'magic_shell'
