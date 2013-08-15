378b2764d67288ad08ae83a431633761f2dab459
Josh Gagnon <jgagnon@rsa.com>
2013-08-15 15:53:56 -0400

    Tweaking the role to test updating on the chef server.

--- roles/starter.rb
+++ roles/starter.rb
@@ -1,6 +1,10 @@
 name "starter"
-description "An example Chef role"
+description "An example Chef role, where it basically just prints my name."
 run_list "recipe[starter]"
+default_attributes({
+    "whocares" => "Seriously, who cares?",
+    "another" => "yayyyy"
+})
 override_attributes({
   "starter_name" => "Josh Gagnon",
 })
