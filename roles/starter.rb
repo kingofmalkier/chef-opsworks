049fb89d17a771d388a7c7f64559072086b5b90c
Josh Gagnon <jgagnon@rsa.com>
2013-08-15 19:27:09 -0400

    Rolling back to a less silly version.

--- roles/starter.rb
+++ roles/starter.rb
@@ -1,10 +1,6 @@
 name "starter"
-description "An example Chef role, where it basically just prints my name."
+description "An example Chef role"
 run_list "recipe[starter]"
-default_attributes({
-    "whocares" => "Seriously, who cares?",
-    "another" => "yayyyy"
-})
 override_attributes({
   "starter_name" => "Josh Gagnon",
 })
